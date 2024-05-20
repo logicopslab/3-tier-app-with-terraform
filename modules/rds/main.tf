44f4cec593c04240e3ea0cb64afb6f798fcb5dbe
logicopslab <logicopslab@gmail.com>
2024-05-20 20:30:28 +0530

    Skeleton of RDS code

---
+++ modules/rds/main.tf
@@ -0,0 +1,16 @@
+resource "aws_db_subnet_group" "main" {
+  name = "main-subnet-group"
+  subnet_ids = var.subnet_ids
+}
+
+resource "aws_db_instance" "main" {
+  identifier = "main-db"
+  engine = "mysql"
+  instance_class = "db.t2.micro"
+  allocated_storage = 20
+  db_subnet_group_name = aws_db_subnet_group.main.name
+  vpc_security_group_ids = [var.security_group_id]
+  username = var.db_username
+  password = var.db_password
+  skip_final_snapshot = true
+}
---
+++ modules/rds/variables.tf
@@ -0,0 +1,6 @@
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "security_group_id" {}
+variable "db_username" {}
+variable "db_password" {}
